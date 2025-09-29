8f5199eddf0fc3d007eedabb801ca3c406bde8c7
Rocha072 <matheusrochaalmeida7@gmail.com>
2025-09-29 18:25:52 -0300

    Analisador de e-emails pronto

---
+++ backend/main.py
@@ -0,0 +1,153 @@
+#IMPORTAÇÕES
+
+#Bibliotecas padrão
+import os 
+from dotenv import load_dotenv
+import re
+import json
+
+# Spacy para o nlp 
+import spacy
+
+#Biblioteca do Gemini
+import google.generativeai as genai     
+
+#Bibliotecas para utilizar o FastAPI
+from fastapi import FastAPI, File, UploadFile, Form, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+#Bibliteca auxiliar
+from typing import Optional
+
+#Biblioteca para extracao de texto de PDF
+import PyPDF2
+from io import BytesIO
+
+from prompt_gemini import prompt
+
+#Carrega os valores do .env
+load_dotenv()
+
+#Anexacao da api key ao genai
+genai.configure(api_key=os.getenv("API_KEY_GEMINI")) 
+
+#Escolha do modelo da IA do Gemini
+model = genai.GenerativeModel('gemini-flash-latest')
+
+#Criação da FastAPI
+app = FastAPI()
+
+#Carrega o modelo de linguagem do spaCy
+nlp = spacy.load("pt_core_news_sm")
+
+origins = [
+    "https://email-classificator.vercel.app",
+    "http://127.0.0.1:5500",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+
+#Configuracao do CROS (permissoes de chegada)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
+#Funcao para leitura de pdf
+def read_pdf_text(file_bytes):
+
+    #try para leitura do pdf para caso do pdf estar corrompido
+    try:
+        pdf_file = BytesIO(file_bytes)  #Criacao de um arquivo virtual (para poupar de criar um arquivo)
+        
+        reader = PyPDF2.PdfReader(pdf_file) #Criacao do leitor de PDF
+        txt = ""
+
+        for page in reader.pages:  #Percorre cada pagina do PDF
+            txt+=page.extract_text()
+        
+        return txt
+    
+    except Exception as e:
+        print("Erro ao ler PDF: " + e)
+        return ""
+
+
+#Realiza o pre processamento NLP com remocao de stop words e lemmatização
+
+def preprocess_nlp(text):
+    
+    text = re.sub(r"http\S+", "", text).lower() #Remove links
+    text = " ".join(text.split())
+
+    doc = nlp(text)       #Realiza a lemmatização e retorna tokens
+    
+    processed_words = [
+        token.lemma_.lower() for token in doc if not token.is_stop and not token.is_punct and token.is_alpha    #Forma as palavras processadas, sem stop words e pontos, apenas palavras
+    ]
+    
+    return " ".join(processed_words)
+
+
+#Funcao post da fastAPI com decorador
+@app.post("/analyze-email")
+async def analyze_email_endpoint(       #Funcao assincrona de endpoint para analise do email
+    text: Optional[str] = Form(None),       #Se existir text será string, senão none
+    file: Optional[UploadFile] = File(None) #Se existir file será UploadFile, senão none
+):
+    email_to_analyze = ""
+
+    #Se for um arquivo
+    if file:
+        content_file = await file.read() #Espera a leitura estar completa e salva em bytes
+
+        if file.content_type == 'application/pdf':  
+            email_to_analyze = read_pdf_text(content_file) #Se for pdf, lê o pdf
+        
+        else:
+            raise HTTPException(status_code=400, detail="Tipo de arquivo não suportado. Somente .txt ou .pdf são aceitos")
+    
+
+    #Se for texto apenas coloca no email de analise
+    elif text:
+        email_to_analyze = text
+    
+
+    #Se nao houver email para anisar, erro.
+    if not email_to_analyze.strip():
+        raise HTTPException(status_code=400, detail="Nenhum texto ou arquivo válido foi enviado")
+
+
+    #Realiza o pre processamento, manda o prompt completo para o modelo e retorna a resposta.
+    try:
+
+        processedEmail = preprocess_nlp(email_to_analyze)
+
+        finalPrompt = prompt + f"""
+                                ---
+                                {processedEmail}
+                                ---
+                                JSON:
+                                """ 
+
+        response = model.generate_content(finalPrompt)
+
+        #Caso tenha sido colocado ``` na resposta, retira-se para a saída sair devidamente.
+        response_clean = response.text.strip().replace("```json", "").replace("```", "")
+
+        #Converte a string json para objeto python
+        response_object = json.loads(response_clean)
+
+        return {"data": response_object}
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+
+
+
---
+++ frontend/scripts/main.js
@@ -0,0 +1,204 @@
+
+// Declaração de todos os elementos utilizados
+
+const formulary = document.getElementById('form-input');
+
+const emailTextArea = document.getElementById('email-text');
+const fileOnEmailText = document.getElementById('file-on-email-text')
+const removeFileBtn = document.getElementById('remove-file-btn');
+
+const fileInput = document.getElementById('file-input');
+const fileNameDisplay = document.getElementById('file-name-display');
+
+const classification = document.getElementById('classification-output');
+const suggestion = document.getElementById('suggested-output');
+const copyBtn = document.getElementById('copy-button');
+
+// ------------------------------------------------------
+//                  FUNÇÕES AUXILIARES
+// ------------------------------------------------------
+
+
+// Função para ler e mostrar o arquivo .txt na área de texto
+const displayTextFromFile = (file) => {
+
+    const txtReader = new FileReader();
+    
+    txtReader.onload = (f) => {         //Definição da função que lê o file
+        emailTextArea.value = f.target.result;  //Coloca na textArea
+        fileInput.value = null;                 //Garante que não tenha outro arquivo no input
+        emailTextArea.readOnly = false;         //Mantem ativada a text area
+    };
+    
+    txtReader.readAsText(file)      
+}
+
+
+// Mostra a tag de file na caixa de texto
+const displayFileTag = (file) => {
+    fileNameDisplay.textContent = file.name;
+    fileOnEmailText.classList.add('visible');
+    emailTextArea.readOnly = true;
+    emailTextArea.value = '';
+    emailTextArea.placeholder = ''
+}
+
+// Remove o arquivo da caixa de texto
+const removeFileFromText = () => {
+    fileInput.value = null;
+    fileOnEmailText.classList.remove('visible');
+    emailTextArea.readOnly = false;
+    emailTextArea.value = '';
+    emailTextArea.placeholder = 'Escreva ou arraste o arquivo aqui';
+}
+
+// Seleciona a ação dependendo do tipo de arquivo recebido
+const selectAction_FileType = (file) => {
+
+    // Se for txt mostra na text area
+    if(file.type == 'text/plain'){  
+        removeFileFromText();
+        displayTextFromFile(file);
+    }
+
+    // Se for pdf mostra a tag de arquivo
+    else if(file.type == 'application/pdf'){
+        displayFileTag(file);
+    }
+
+    // Caso contrário, alerta de seleção errada
+    else{
+        alert('Tipo de arquivo não suportado. Por favor, use .txt ou .pdf');
+    }
+}
+
+// ------------------------------------------------------
+//                  EVENT LISTENERS
+// ------------------------------------------------------
+
+
+//Listener pro botão de remoção da tag de arquivo
+removeFileBtn.addEventListener('click', removeFileFromText);
+
+
+//Text area do email pinta as bordas quando o arquivo esta em cima
+emailTextArea.addEventListener('dragover', (event) => {
+    event.preventDefault();
+    emailTextArea.classList.add('drag-over');
+});
+
+
+//Text area do email volta ao normal quando arquivo nao está mais em cima
+emailTextArea.addEventListener('dragleave', ()=>{
+    emailTextArea.classList.remove('drag-over');
+});
+
+
+//Text area recebe arquivo soltado
+emailTextArea.addEventListener('drop', (event)=>{
+
+    event.preventDefault();
+    emailTextArea.classList.remove('drag-over'); 
+
+    const files = event.dataTransfer.files;
+
+    if(files.length > 0){
+        const fileDropped = files[0]; 
+        fileInput.files = files;            //Coloca arquivo no fileInput
+        selectAction_FileType(fileDropped); //Determina a parte visual de retorno para usuário
+    }
+});
+
+//Arquivo pode ser anexado pelo botao de anexar
+fileInput.addEventListener('change', ()=>{
+
+    if(fileInput.files.length > 0){
+        selectAction_FileType(fileInput.files[0]);   //Determina a parte visual de retorno para usuário
+    } 
+});
+
+
+copyBtn.addEventListener('click', async ()=>{
+    
+    const tempTextArea = document.createElement('textarea');
+    tempTextArea.value = suggestion.textContent;
+    tempTextArea.style.position = 'absolute';
+    tempTextArea.style.left = '-9999px';
+    document.body.appendChild(tempTextArea);
+
+    tempTextArea.select();
+    try{
+        document.execCommand('copy');
+    }catch(e){
+        console.error(e)
+    }
+});
+
+
+//Listener para enviar o formulário para o backend através do botão de análise
+formulary.addEventListener('submit', async(event)=>{ //Evento deve ser assincrono pois demora alguns segundos
+    event.preventDefault();
+    
+    const file = fileInput.files[0];
+    const emailText = emailTextArea.value;
+
+    //Se tentou enviar sem conteudo, aviso.
+    if(!emailText.trim() && !file){ 
+        alert('Por favor, digite um texto ou selecione um arquivo.');
+        return;
+    }
+
+    //FormData para receber o conteudo enviado (pdf ou texto)
+    const formData = new FormData();
+
+    if(file){
+        formData.append('file', file);
+    } else {
+        formData.append('text', emailText);
+    }
+
+    //Atualiza os textos para feedback
+    classification.textContent = 'Analisando...';
+    suggestion.textContent = 'Aguarde...';
+    copyBtn.classList.remove('visible');
+
+    //Comunicacao com a API do python
+    try{
+        const apiURL = 'https://email-analyzer-api-45f0.onrender.com/analyze-email';
+
+        //Envio do conteudo em formato json com método post
+        const response = await fetch(apiURL, {
+            method: 'POST',
+            body: formData,
+        });
+
+        //Confere se não deu erro
+        if(!response.ok){
+            throw new Error(`Erro na requisição: ${response.statusText}`);
+        }
+
+        //Abre o pacote da resposta de forma assíncrona
+        const result = await response.json();
+
+        //Verifica se não deu erro na busca do json
+        if(result.error){
+            throw new Error(result.error);
+        }
+
+        // Atualiza os textos com a resposta da IA
+        classification.textContent = result.data.classification;
+        suggestion.textContent = result.data.suggested;
+
+        copyBtn.classList.add('visible');
+
+        //Verifica se não houve outros erros no meio
+    } catch(error){
+        console.error('Falha ao analisar o e-mail', error);
+        classification.textContent = 'Erro';
+        suggestion.textContent = `Ocorreu um erro ${error.message}`;
+    }
+
+});
+
+
+
